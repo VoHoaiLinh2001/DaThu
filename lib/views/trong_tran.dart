38c254684a12c4d55a67d4b13e2c58a649f82a66
Phước Giàu <0306201228@caothang.edu.vn>
2022-11-11 04:36:01 +0700

    trong_tran.dart full

--- lib/views/trong_tran.dart
+++ lib/views/trong_tran.dart
@@ -44,31 +44,31 @@ class _trongtranState extends State<trongtran> {
                     Container(
                         child: IconButton(
                       icon: Image.asset('asset/1024px-Refresh_icon.png'),
-                      iconSize: 50,
+                      iconSize: 40,
                       onPressed: () {},
                     )),
                     Container(
                         child: IconButton(
                       icon: Image.asset('asset/zhucheng_liaotian.png'),
-                      iconSize: 50,
+                      iconSize: 40,
                       onPressed: () {},
                     )),
                     Container(
                         child: IconButton(
                       icon: Image.asset('asset/zhucheng_beibao.png'),
-                      iconSize: 50,
+                      iconSize: 40,
                       onPressed: () {},
                     )),
                     Container(
                         child: IconButton(
                       icon: Image.asset('asset/huanpi_ZD_meishuzi3.png'),
-                      iconSize: 50,
+                      iconSize: 40,
                       onPressed: () {},
                     )),
                     Container(
                         child: IconButton(
                       icon: Image.asset('asset/ZZZZZ.png'),
-                      iconSize: 50,
+                      iconSize: 40,
                       onPressed: () {},
                     ))
                   ],
@@ -83,7 +83,7 @@ class _trongtranState extends State<trongtran> {
                         fit: BoxFit.fill)),
                 child: Container(
                     height: (MediaQuery.of(context).size.height) -
-                        ((MediaQuery.of(context).size.height) * 0.22),
+                        ((MediaQuery.of(context).size.height) * 0.18),
                     child: Column(
                         //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -101,7 +101,7 @@ class _trongtranState extends State<trongtran> {
                             ],
                           ),
                           Container(
-                            height: MediaQuery.of(context).size.height / 2.2,
+                            height: MediaQuery.of(context).size.height / 2.9,
                             width: MediaQuery.of(context).size.width / 1.2,
                             decoration: const BoxDecoration(
                               image: DecorationImage(
@@ -136,7 +136,7 @@ class _trongtranState extends State<trongtran> {
                                     padding: EdgeInsets.only(top: 60),
                                     margin: EdgeInsets.only(left: 5),
                                     height: 100,
-                                    width: 50,
+                                    width: 40,
                                     decoration: BoxDecoration(
                                         image: DecorationImage(
                                             image: AssetImage(
@@ -148,8 +148,95 @@ class _trongtranState extends State<trongtran> {
                                             color: Colors.white),
                                         textAlign: TextAlign.center)),
                               ]),
-                              Container(child: Text('Câu hỏi')),
+                              Container(
+                                  child: Text('Câu hỏi',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
                             ]),
+                          ),
+                          Container(
+                            width: 300,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('asset/hd_kapai44.png'),
+                                    fit: BoxFit.fill)),
+                            child: Container(
+                                margin: EdgeInsets.only(top: 5),
+                                child: Column(children: [
+                                  Text('Đáp án',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'MyFont'),
+                                      textAlign: TextAlign.center),
+                                  Container(
+                                    width: 270,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                                AssetImage('asset/zb_di2.png'),
+                                            fit: BoxFit.fill)),
+                                    child: Container(
+                                        padding:
+                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                        child: Text('A',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ))),
+                                  ),
+                                  Container(
+                                    width: 270,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                                AssetImage('asset/zb_di2.png'),
+                                            fit: BoxFit.fill)),
+                                    child: Container(
+                                        padding:
+                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                        child: Text('A',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ))),
+                                  ),
+                                  Container(
+                                    width: 270,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                                AssetImage('asset/zb_di2.png'),
+                                            fit: BoxFit.fill)),
+                                    child: Container(
+                                        padding:
+                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                        child: Text('A',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ))),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(bottom: 5),
+                                    width: 270,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                                AssetImage('asset/zb_di2.png'),
+                                            fit: BoxFit.fill)),
+                                    child: Container(
+                                        padding:
+                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                        child: Text('A',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ))),
+                                  ),
+                                ])),
                           ),
                         ])),
               ),
