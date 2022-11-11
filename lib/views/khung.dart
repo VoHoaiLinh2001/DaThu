d2304fb6aae607546530bea16956d48a9a3ccd3f
HP <HP@DESKTOP-B5TE9M0>
2022-11-11 12:07:54 +0700

    nhã

---
+++ lib/views/khung.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mauflutter/views/dang_ky.dart';
+// ignore: unused_import
+import 'package:mauflutter/views/info.dart';
+import 'package:mauflutter/views/trangchu.dart';
+class khung extends StatefulWidget {
+  const khung({super.key});
+
+  @override
+  State<khung> createState() => _khung();
+}
+
+class _khung extends State<khung> {
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+        body:Container(
+          child: Column(
+            children: [
+              
+          ]),
+        )
+    );
+   }
+}
