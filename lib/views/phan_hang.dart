da76b0941ec4b2241191ff4c33581c0fc844b16d
Phước Giàu <0306201228@caothang.edu.vn>
2022-11-17 21:07:26 +0700

    phan_hang.dart

--- lib/views/phan_hang.dart
+++ lib/views/phan_hang.dart
@@ -19,124 +19,199 @@ class _phanhangState extends State<phanhang> {
           image: DecorationImage(
               image: AssetImage("asset/bg_shen.jpg"), fit: BoxFit.cover),
         ),
-        child: ListView.builder(
-          itemCount: db_context.items.length,
-          itemBuilder: (context, index) {
-            return Info_rank_frame(
-              info: db_context.items[index],
-            );
-          },
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage('asset/half_347.png'),
+                      fit: BoxFit.cover,
+                    ),
+                    border: Border.all(color: Colors.yellow, width: 2),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 20, left: 5),
+                      padding: EdgeInsets.only(left: 15, right: 15),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("asset/jiuguan4b.png"),
+                            fit: BoxFit.cover),
+                      ),
+                      child: Text(
+                        'Doãn Chí Bình',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image:
+                                AssetImage("asset/huanpi_gongyong_anniu_0.png"),
+                            fit: BoxFit.cover),
+                      ),
+                      child: Text(
+                        'Cảnh giới',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 60,
+                  height: 60,
+                  margin: EdgeInsets.only(left: 15, right: 15, top: 20),
+                  child: Image(
+                    image: AssetImage('asset/icon_duanwei_06.png'),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 20,
+                          padding: EdgeInsets.only(left: 10, right: 10),
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("asset/icon_dian1.png"),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        Text(
+                          '123456',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 20,
+                          padding: EdgeInsets.only(left: 10, right: 10),
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("asset/icon_dian0.png"),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        const Text(
+                          '123456',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Column(children: [
+              Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 5),
+                  padding: EdgeInsets.fromLTRB(140, 8, 140, 0),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("asset/sdk_notice_title_bg.png"),
+                        fit: BoxFit.fill),
+                  ),
+                  child: Column(
+                    children: [
+                      Text('Bảng xếp hạng',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 30))
+                    ],
+                  )),
+              Column(children: [
+                Container(
+                    //shouchonglibao_anniu.png
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("asset/bg_mianban_big2.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(7))),
+                    height: MediaQuery.of(context).size.height * 0.75,
+                    child: Column(children: [
+                      Row(children: [
+                        Expanded(
+                            child: Container(
+                          margin: EdgeInsets.fromLTRB(40, 5, 40, 0),
+                          padding: EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    "asset/shouchonglibao_anniu.png"),
+                                fit: BoxFit.fill),
+                          ),
+                          child: TextButton(
+                            child: const Text('CẢNH GIỚI',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white)),
+                            onPressed: () {
+                              /*  Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: ((context) => const ()),
+                                ),
+                              ); */
+                            },
+                          ),
+                        )),
+                        Expanded(
+                            child: Container(
+                          margin: EdgeInsets.fromLTRB(40, 5, 40, 0),
+                          padding: EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    "asset/shouchonglibao_anniu.png"),
+                                fit: BoxFit.fill),
+                          ),
+                          child: TextButton(
+                            child: const Text('XẾP HẠNG',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white)),
+                            onPressed: () {
+                              /*  Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: ((context) => const ()),
+                                ),
+                              ); */
+                            },
+                          ),
+                        ))
+                      ]),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: db_context.items.length,
+                          itemBuilder: (context, index) {
+                            return Info_rank_frame(
+                              info: db_context.items[index],
+                            );
+                          },
+                        ),
+                      )
+                    ]))
+              ])
+            ]),
+          ],
         ),
-      ),
-    );
-    body:
-    Container(
-      padding: EdgeInsets.all(5),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage("asset/bg_shen.jpg"), fit: BoxFit.cover),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                image: AssetImage('asset/half_347.png'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(color: Colors.yellow, width: 2),
-              borderRadius: BorderRadius.circular(5),
-            ),
-          ),
-          Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 20, left: 5),
-                padding: EdgeInsets.only(left: 15, right: 15),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("asset/jiuguan4b.png"),
-                      fit: BoxFit.cover),
-                ),
-                child: Text(
-                  'Doãn Chí Bình',
-                  style: const TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 5),
-                padding: EdgeInsets.only(left: 20, right: 20),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("asset/huanpi_gongyong_anniu_0.png"),
-                      fit: BoxFit.cover),
-                ),
-                child: Text(
-                  'Cảnh giới',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            width: 60,
-            height: 60,
-            margin: EdgeInsets.only(left: 15, right: 15, top: 20),
-            child: Image(
-              image: AssetImage('asset/icon_duanwei_06.png'),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 20,
-                    width: 20,
-                    padding: EdgeInsets.only(left: 10, right: 10),
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("asset/icon_dian1.png"),
-                          fit: BoxFit.cover),
-                    ),
-                  ),
-                  Text(
-                    '123456',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    height: 20,
-                    width: 20,
-                    padding: EdgeInsets.only(left: 10, right: 10),
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("asset/icon_dian0.png"),
-                          fit: BoxFit.cover),
-                    ),
-                  ),
-                  const Text(
-                    '123456',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  )
-                ],
-              ),
-            ],
-          ),
-          ListView.builder(
-              itemCount: db_context.items.length,
-              itemBuilder: (context, index) {
-                return Info_rank_frame(info: db_context.items[index]);
-              })
-        ],
       ),
     );
   }
