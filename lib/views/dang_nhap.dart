3a02135848b0b935c10ee15ada8d37b48533df4a
Phước Giàu <0306201228@caothang.edu.vn>
2022-11-30 14:54:10 +0700

    fix dinh dang dang nhap, dang ky

--- lib/views/dang_nhap.dart
+++ lib/views/dang_nhap.dart
@@ -25,152 +25,154 @@ class _dangnhapState extends State<dangnhap> {
               ),
               fit: BoxFit.cover),
         ),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 0,
-              child: Container(
-                child: Text(
-                  'Đăng Nhập',
-                  style: TextStyle(fontSize: 30),
+        child: ListView(children: [
+          Column(
+            children: [
+              Expanded(
+                flex: 0,
+                child: Container(
+                  child: Text(
+                    'Đăng Nhập',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  margin: EdgeInsets.only(top: 200),
                 ),
-                margin: EdgeInsets.only(top: 200),
               ),
-            ),
-            Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(75, 10, 75, 0),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(75, 10, 75, 0),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(75, 10, 75, 0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Mật khẩu',
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(75, 10, 75, 0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Mật khẩu',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Checkbox(
-                  value: this.checkBoxValue,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      this.checkBoxValue = value!;
-                    });
-                  },
-                ),
-                Text('Ghi nhớ đăng nhập')
-              ],
-            ),
-            Expanded(
-              flex: 0,
-              child: Row(
+                ],
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    margin: EdgeInsets.only(right: 10),
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            'asset/button.png',
-                          ),
-                          fit: BoxFit.cover),
-                    ),
-                    width: 140,
-                    height: 50,
-                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: TextButton(
-                      child: const Text(
-                        'Đăng nhập',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => const trangchu()),
-                          ),
-                        );
-                      },
-                    ),
+                  Checkbox(
+                    value: this.checkBoxValue,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        this.checkBoxValue = value!;
+                      });
+                    },
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10),
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            'asset/button.png',
-                          ),
-                          fit: BoxFit.cover),
-                    ),
-                    width: 140,
-                    height: 50,
-                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    child: TextButton(
-                      child: const Text(
-                        'Đăng ký',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => const dangky()),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                  Text('Ghi nhớ đăng nhập')
                 ],
               ),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 25, bottom: 25),
-                    child: Text(
-                      'Đăng nhập bằng',
+              Expanded(
+                flex: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 10),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              'asset/button.png',
+                            ),
+                            fit: BoxFit.cover),
+                      ),
+                      width: 140,
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: TextButton(
+                        child: const Text(
+                          'Đăng nhập',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: ((context) => const trangchu()),
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        icon: Image.asset('asset/fb.png'),
-                        iconSize: 50,
-                        onPressed: () {},
+                    Container(
+                      margin: EdgeInsets.only(left: 10),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              'asset/button.png',
+                            ),
+                            fit: BoxFit.cover),
                       ),
-                      IconButton(
-                        icon: Image.asset('asset/gg.jpg'),
-                        iconSize: 50,
-                        onPressed: () {},
+                      width: 140,
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                      child: TextButton(
+                        child: const Text(
+                          'Đăng ký',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: ((context) => const dangky()),
+                            ),
+                          );
+                        },
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 25, bottom: 25),
+                      child: Text(
+                        'Đăng nhập bằng',
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Image.asset('asset/fb.png'),
+                          iconSize: 50,
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Image.asset('asset/gg.jpg'),
+                          iconSize: 50,
+                          onPressed: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ]),
       ),
     );
   }
