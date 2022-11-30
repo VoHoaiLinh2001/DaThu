e84559611f891852fe25e1e59280f331a187148c
Phước Giàu <0306201228@caothang.edu.vn>
2022-11-30 12:54:38 +0700

    trong_tran.dart

--- lib/views/trong_tran.dart
+++ lib/views/trong_tran.dart
@@ -85,161 +85,150 @@ class _trongtranState extends State<trongtran> {
                 child: Container(
                     height: (MediaQuery.of(context).size.height) -
                         ((MediaQuery.of(context).size.height) * 0.18),
-                    child: Column(
-                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Column(children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15),
+                              child: Text(
+                                'Điểm số',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ))
+                        ],
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height / 2.9,
+                        width: MediaQuery.of(context).size.width / 1.2,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                'asset/shouchonglibao.png',
+                              ),
+                              fit: BoxFit.fill),
+                        ),
+                        child: Column(children: [
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                  margin: EdgeInsets.only(top: 15),
-                                  child: Text(
-                                    'Điểm số',
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.only(top: 6),
+                                  margin: EdgeInsets.only(top: 4),
+                                  height: 30,
+                                  width: 90,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'asset/shouchonglibao_anniu.png'),
+                                          fit: BoxFit.fill)),
+                                  child: Text('1/10',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                      textAlign: TextAlign.center),
+                                )
+                              ]),
+                          Row(children: [
+                            Container(
+                                padding: EdgeInsets.only(top: 60),
+                                margin: EdgeInsets.only(left: 5),
+                                height: 100,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'asset/xuanzhongguanka.png'),
+                                        fit: BoxFit.fill)),
+                                child: Text('00:20',
                                     style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 20),
-                                  ))
-                            ],
-                          ),
+                                        color: Colors.white),
+                                    textAlign: TextAlign.center)),
+                          ]),
                           Container(
-                            height: MediaQuery.of(context).size.height / 2.9,
-                            width: MediaQuery.of(context).size.width / 1.2,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage(
-                                    'asset/shouchonglibao.png',
-                                  ),
-                                  fit: BoxFit.fill),
-                            ),
+                              child: Text('Câu hỏi',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ))),
+                        ]),
+                      ),
+                      Container(
+                        width: 300,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('asset/hd_kapai44.png'),
+                                fit: BoxFit.fill)),
+                        child: Container(
+                            margin: EdgeInsets.only(top: 5),
                             child: Column(children: [
-                              Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Container(
-                                      padding: EdgeInsets.only(top: 6),
-                                      margin: EdgeInsets.only(top: 4),
-                                      height: 30,
-                                      width: 90,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              image: AssetImage(
-                                                  'asset/shouchonglibao_anniu.png'),
-                                              fit: BoxFit.fill)),
-                                      child: Text('1/10',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.white),
-                                          textAlign: TextAlign.center),
-                                    )
-                                  ]),
-                              Row(children: [
-                                Container(
-                                    padding: EdgeInsets.only(top: 60),
-                                    margin: EdgeInsets.only(left: 5),
-                                    height: 100,
-                                    width: 40,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: AssetImage(
-                                                'asset/xuanzhongguanka.png'),
-                                            fit: BoxFit.fill)),
-                                    child: Text('00:20',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white),
-                                        textAlign: TextAlign.center)),
-                              ]),
+                              Text('Đáp án',
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'MyFont'),
+                                  textAlign: TextAlign.center),
                               Container(
-                                  child: Text('Câu hỏi',
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ))),
-                            ]),
-                          ),
-                          Container(
-                            width: 300,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage('asset/hd_kapai44.png'),
-                                    fit: BoxFit.fill)),
-                            child: Container(
-                                margin: EdgeInsets.only(top: 5),
-                                child: Column(children: [
-                                  Text('Đáp án',
-                                      style: TextStyle(
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'MyFont'),
-                                      textAlign: TextAlign.center),
-                                  Container(
-                                    width: 270,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image:
-                                                AssetImage('asset/zb_di2.png'),
-                                            fit: BoxFit.fill)),
-                                    child: Container(
-                                        padding:
-                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
-                                        child: Text('A',
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                            ))),
-                                  ),
-                                  Container(
-                                    width: 270,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image:
-                                                AssetImage('asset/zb_di2.png'),
-                                            fit: BoxFit.fill)),
-                                    child: Container(
-                                        padding:
-                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
-                                        child: Text('B',
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                            ))),
-                                  ),
-                                  Container(
-                                    width: 270,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image:
-                                                AssetImage('asset/zb_di2.png'),
-                                            fit: BoxFit.fill)),
-                                    child: Container(
-                                        padding:
-                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
-                                        child: Text('C',
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                            ))),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(bottom: 5),
-                                    width: 270,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image:
-                                                AssetImage('asset/zb_di2.png'),
-                                            fit: BoxFit.fill)),
-                                    child: Container(
-                                        padding:
-                                            EdgeInsets.fromLTRB(55, 13, 0, 0),
-                                        child: Text('D',
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                            ))),
-                                  ),
-                                ])),
-                          ),
-                        ])),
+                                width: 270,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('asset/zb_di2.png'),
+                                        fit: BoxFit.fill)),
+                                child: Container(
+                                    padding: EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                    child: Text('A',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                        ))),
+                              ),
+                              Container(
+                                width: 270,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('asset/zb_di2.png'),
+                                        fit: BoxFit.fill)),
+                                child: Container(
+                                    padding: EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                    child: Text('B',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                        ))),
+                              ),
+                              Container(
+                                width: 270,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('asset/zb_di2.png'),
+                                        fit: BoxFit.fill)),
+                                child: Container(
+                                    padding: EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                    child: Text('C',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                        ))),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 5),
+                                width: 270,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('asset/zb_di2.png'),
+                                        fit: BoxFit.fill)),
+                                child: Container(
+                                    padding: EdgeInsets.fromLTRB(55, 13, 0, 0),
+                                    child: Text('D',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                        ))),
+                              ),
+                            ])),
+                      ),
+                    ])),
               ),
             ])));
   }
